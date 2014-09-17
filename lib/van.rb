c1db735eaf256ee7fb080fc0b40988a41008278f
Anna Jerofejeva <annajerofejeva@Annas-MacBook-Pro.local>
2014-09-17 16:39:05 +0100

    Added van accepts from station

--- lib/van.rb
+++ lib/van.rb
@@ -1,4 +1,5 @@
 require_relative 'bike_container'
+require_relative 'docking_station'
 
 include BikeContainer
 
@@ -7,8 +8,13 @@ class Van
   	  self.capacity = options.fetch(:capacity, capacity)
     end
 
-    def release_broken_bikes
+    def release_to_garage
       bikes.delete(broken_bikes.pop)
+    end
+
+    def accept_from_station(station)
+    bikes << station.release_to_van until station.broken_bikes.empty?
+    raise "The van is full!" if full?
     end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,6 +3,8 @@ require 'van'
 describe Van do
 	
   let(:van) { Van.new(:capacity => 123) }
+  let(:station) { DockingStation.new(:capacity => 20)}
+  let(:bike) {Bike.new}
 
   it "should allow setting default capacity on initialising" do
     expect(van.capacity).to eq(123)
@@ -13,8 +15,27 @@ describe Van do
     broken_bike = Bike.new
     broken_bike.break!
     van.dock(broken_bike)
-    van.release_broken_bikes
+    van.release_to_garage
     expect(van.bike_count).to eq(0)
-    
   end
+
+  it "should release working bikes to the docking station" do
+  	expect(station).to be_empty
+  	van.dock(bike)
+  	expect(van.broken_bikes).to be_empty
+  	van.release
+  	station.dock(bike)
+  	expect(van.bike_count).to eq(0)
+  	expect(station.bike_count).to eq(1)
+  end
+   it "should accept broken bikes from the station" do
+   	broken_bike = Bike.new
+   	broken_bike.break!
+   	station.dock(broken_bike)
+   	expect(van.bike_count).to eq(0)
+   	van.accept_from_station(station)
+   	expect(van.bike_count).to eq(1)
+   end
+
+  	
 end
