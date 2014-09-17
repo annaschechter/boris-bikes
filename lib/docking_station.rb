f52509785d0aaba0018c0f3d2c12595683dfb5bc
Anna Jerofejeva <annajerofejeva@Annas-MacBook-Pro.local>
2014-09-17 12:47:09 +0100

    Added 'empty' methods to docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,4 +8,12 @@ class DockingStation
   	self.capacity = options.fetch(:capacity, capacity)
   end
 
+  def empty?
+  	bike_count == 0
+  end
+
+  def has_rentals?
+  	!available_bikes.empty?
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,5 +8,15 @@ it "should allow setting default capacity on initialising" do
   expect(station.capacity).to eq(123)
 end
 
+
+it "should know when there are no bikes" do
+	expect(station.bike_count).to eq(0)
+	expect(station).to be_empty
+end
+
+it "should know when there are no bikes available" do
+	expect(available_bikes).to be_empty
+	expect(station).not_to have_rentals
+end
 	
 end
