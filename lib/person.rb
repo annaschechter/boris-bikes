24ca7fcc1f795923f24926882e8b9574daa1fbba
Anna Jerofejeva <annajerofejeva@Annas-MacBook-Pro.local>
2014-09-18 13:48:28 +0100

    Now person can rent a bike

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,18 @@
+require_relative 'bike'
+require_relative 'docking_station'
+
 class Person
 
-	def initialize
-		@bike = false
+	def initialize(bike=nil)
+		@bike = bike
 	end
 
 	def has_bike?
-		@bike 
+		@bike != nil
 	end
+
+	def rent_a_bike(station)
+        @bike = station.release
+	end
+
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,9 +3,18 @@ require 'person'
 describe Person do 
 
 	let(:person) {Person.new}
+	let(:station) {DockingStation.new}
+	let(:bike) {Bike.new}
 	
 	it "should not possess a bike" do
 	   expect(person).not_to have_bike
 	end
-		
+
+	it "should be able to rent a bike" do
+		station.dock(bike)
+		person.rent_a_bike(station)
+		expect(person).to have_bike
+	end
+
+
 end
